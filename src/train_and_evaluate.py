a02ba3c38d108f0ec8a67da53a94fd24d6bba275
Robinson Panicker <robinson.panicker@oracle.com>
2022-03-23 15:00:22 +0530

    adding metrics for report code

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -54,6 +54,11 @@ def train_and_evaluate(config_path):
     print("MAE: %s"%mae)
     print("R2:%s"%r2)
 
+    ####################
+    os.makedirs(model_dir, exist_ok=True)
+    model_path=os.path.join(model_dir, "model.joblib")
+    joblib.dump(lr, model_path)
+
 if __name__ == "__main__":
     args=argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
