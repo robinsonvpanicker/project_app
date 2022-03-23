51fb36acd0f8bbdbb56275e7d33a4952cfe0b646
Robinson Panicker <robinson.panicker@oracle.com>
2022-03-23 17:09:01 +0530

    tox testing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version="0.0.0",
+    description="Wine package demo",
+    author="Dr.Rao",
+    packages=find_packages(),
+    license="MIT"
+    )
+    
